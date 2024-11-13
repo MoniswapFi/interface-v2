aa9c8be3811f34f925f052b7861cc7ed576c501f
5eh <watsonlewrod1@gmail.com>
2024-11-12 22:43:51 -0600

    :zap: Worked more on the home page :zap:

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./styles/index.css";
 
 function App() {
@@ -22,27 +23,100 @@ function App() {
           </div>
 
           {/* Stats */}
-          <div className="max-w-4xl bg-red-300/20 border border-red-400">
-            <div className="grid">
-              <div>
-                <p> $22.7M </p>
-                <p> Total Unlocked </p>
-              </div>
+          <div className="flex items-center justify-center w-full">
+            <div className="max-w-7xl bg-primary-gr/20 p-4">
+              <div className="grid grid-cols-2 bg-blue-300/10  rounded-lg gap-x-24">
+                <div className="text-center p-4 rounded-lg px-12 py-8">
+                  <p className="text-2xl font-medium primary-gradient">
+                    $22.7M
+                  </p>
+                  <p className="text-sm text-white"> Total Value Unlocked </p>
+                </div>
 
-              <div>
-                <p> $22.7M </p>
-                <p>Total Trading Volume</p>
-              </div>
+                <div className="text-center p-4 rounded-lg px-12 py-8">
+                  <p className="text-2xl font-medium primary-gradient ">
+                    $337.8M
+                  </p>
+                  <p className="text-sm text-white"> Total Trading Volume </p>
+                </div>
 
-              <div>
-                <p> $22.7M </p>
-                <p> Trading Fees</p>
-              </div>
+                <div className="text-center p-4 rounded-lg px-12 py-8">
+                  <p className="text-2xl font-medium primary-gradient">
+                    $685.4K
+                  </p>
+                  <p className="text-sm text-white"> Trading Fees </p>
+                </div>
 
-              <div>
-                <p> $22.7M </p>
-                <p> Active Users</p>
+                <div className="text-center p-4 rounded-lg px-12 py-8">
+                  <p className="text-2xl font-medium primary-gradient">21730</p>
+                  <p className="text-sm text-white"> Active Users </p>
+                </div>
               </div>
+            </div>
+          </div>
+
+          {/* How it works */}
+          <div className="w-full justify-center text-center text-2xl my-12 lg:mb-12 lg:mt-24">
+            <h2 className="primary-gradient"> How it works </h2>
+          </div>
+
+          <div className="flex items-center justify-center w-full">
+            <div className="max-w-4xl p-4">
+              <div className="grid grid-cols-2 gap-x-8 gap-y-6">
+                <div className="text-left p-4 bg-blue-300/10 rounded-lg px-8">
+                  <p className="text-2xl font-medium primary-gradient my-2">
+                    Trades
+                  </p>
+                  <p className="text-md text-white w-4/5">
+                    Swap tokens with minimal slippage and pay some of the lowest
+                    fees to MONI lockers.
+                  </p>
+                </div>
+                <div className="text-left p-4 bg-blue-300/10 rounded-lg px-8">
+                  <p className="text-2xl font-medium primary-gradient my-2">
+                    Liquidity Providers
+                  </p>
+                  <p className="text-md text-white w-4/5">
+                    Deposit the tokens used for trading on Moniswap and receive
+                    MONI emissions as rewards.
+                  </p>
+                </div>
+                <div className="text-left p-4 bg-blue-300/10 rounded-lg px-8">
+                  <p className="text-2xl font-medium primary-gradient my-2">
+                    VeMONI Voters
+                  </p>
+                  <p className="text-md text-white w-full">
+                    Vote on which pools will earn MONI emissions and receive all
+                    incentives and fees. Any MONI holder can lock their tokens
+                    to convert to veMONI.
+                  </p>
+                </div>
+                <div className="text-left p-4 bg-blue-300/10 rounded-lg px-8">
+                  <p className="text-2xl font-medium primary-gradient my-2">
+                    Protocols
+                  </p>
+                  <p className="text-md text-white w-full">
+                    Incentivize veMONI voters to attract votes and MONI
+                    emissions to their pools, enabling them to build liquidity
+                    cost-effectively.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Join the Community */}
+          <div className="w-full justify-center text-center text-2xl my-12 lg:mb-12 lg:mt-24">
+            <h2 className="primary-gradient"> Join the community </h2>
+          </div>
+
+          <div className="w-full justify-center text-center">
+            <div className="">
+              <Link to={`https://discord.com`}>
+                <button className="text-white rounded-md bg-orange-500 px-6 py-3 ">
+                  Join Discord
+                </button>
+              </Link>
             </div>
           </div>
         </div>
